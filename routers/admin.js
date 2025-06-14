7855e877939d70a26b7fdd6120ff92f9d2dc7851
farida3155 <faridahossam19@gmail.com>
2025-06-14 09:08:41 +0300

    Delete routers directory

--- routers/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllEvents, addEvent, deleteEvent } = require('../controllers/admincontroller');
-
-router.get('/events', getAllEvents);
-router.post('/events', addEvent);
-router.delete('/events/:id', deleteEvent);
-
-module.exports = router;
