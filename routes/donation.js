da5ca8514660dc2643dde159321bc07251a6e654
farida3155 <faridahossam19@gmail.com>
2025-06-17 01:00:29 +0300

    Delete routes directory

--- routes/donation.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const donationController = require('../controllers/donationController');
-
-router.post('/donate', donationController.donate);
-
-module.exports = router;
